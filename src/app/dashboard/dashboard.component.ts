aa259835ed74710517cecf565b0af35a526c51b6
mashateayoub <mshtayub@gmail.com>
2025-02-13 21:22:42 +0100

    new Separate components + shared comps + advanced Projection (ngContent) use

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { DashboardItemComponent } from "../shared/dashboard-item/dashboard-item.component";
 import { ServerStatusComponent } from './server-status/server-status.component';
-import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+import { TicketsComponent } from './tickets/tickets.component';
 import { TrafficComponent } from './traffic/traffic.component';
 
 @Component({
 	selector: 'app-dashboard',
 	standalone: true,
-	imports: [ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashboardItemComponent],
+	imports: [ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.css']  // Add this line
 })
