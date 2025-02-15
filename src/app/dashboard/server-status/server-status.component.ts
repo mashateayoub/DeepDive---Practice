a99131572dee8cef490b2b32efeb6bd8acc15c75
mashateayoub <mshtayub@gmail.com>
2025-02-15 19:36:36 +0100

    Component LifeCycle Methods

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,27 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ButtonComponent } from "../../shared/button/button.component";
+import { Component, OnInit } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
-	selector: 'app-server-status',
-	standalone: true,
-	imports: [CommonModule, ButtonComponent],
-	templateUrl: './server-status.component.html',
-	styleUrls: ['./server-status.component.css']
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
+  templateUrl: './server-status.component.html',
+  styleUrls: ['./server-status.component.css'],
 })
-export class ServerStatusComponent {
-	currentStatus: "online" | "offline" | "unknown" = "online";	
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  //   using DestroyRef to destroy the timer
+  //   destroyRef = inject(DestroyRef);
 
-	constructor(){
-		this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
-		setInterval(()=>{
-			this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
-		}, 10000)
-	}
-	
-	refreshStatus(){
-		this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
-	}
+  ngOnInit(): void {
+    this.currentStatus = ['online', 'offline', 'unknown'][
+      Math.floor(Math.random() * 3)
+    ] as 'online' | 'offline' | 'unknown';
+    console.log('Timeout created');
+    // const Interval =
+    setInterval(() => {
+      this.currentStatus = ['online', 'offline', 'unknown'][
+        Math.floor(Math.random() * 3)
+      ] as 'online' | 'offline' | 'unknown';
+    }, 5000);
 
+    // this.destroyRef.onDestroy(() => {
+    //   clearInterval(Interval);
+    //   console.log('Timeout destroyed');
+    // });
+  }
 
+  refreshStatus() {
+    this.currentStatus = ['online', 'offline', 'unknown'][
+      Math.floor(Math.random() * 3)
+    ] as 'online' | 'offline' | 'unknown';
+  }
 }
