a22774e9142f5ee1eeade904871ab6ba7c1da4a0
mashateayoub <mshtayub@gmail.com>
2025-02-15 11:41:05 +0100

    Class Bindings + Styles binding .....

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../../shared/button/button.component";
 
 @Component({
 	selector: 'app-server-status',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, ButtonComponent],
 	templateUrl: './server-status.component.html',
 	styleUrls: ['./server-status.component.css']
 })
 export class ServerStatusComponent {
-	@Input() currentStatus!: string;
+	currentStatus: "online" | "offline" | "unknown" = "online";	
+
+	constructor(){
+		this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
+		setInterval(()=>{
+			this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
+		}, 10000)
+	}
+	
+	refreshStatus(){
+		this.currentStatus = ['online', 'offline', 'unknown'][Math.floor(Math.random() * 3)] as "online" | "offline" | "unknown";
+	}
+
+
 }
